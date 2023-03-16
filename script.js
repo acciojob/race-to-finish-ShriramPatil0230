3d683d6312e5f38cb1ce14220e080336aa289f39
Shriram Patil <shrirampatil246@gmail.com>
2023-03-16 19:41:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,54 +1,26 @@
 window.promises = [];
-promises = [promise1(),promise2(),promise3(),promise4(),promise5()]
 
-Promise.any(promises).then((value) => {
-	document.getElementById("output").innerText = value;
-});
-
-function promise1(){
-	const random = Math.floor(Math.random() *  5) + 1;
-	return new Promise((resolve,reject)=>{
-		setTimeout(()=>{
-		resolve("Promise 1")
-	    },random)
-	})
+for (let i = 0; i < 5; i++) {
+  promises.push(new Promise((resolve) => {
+    const randomTime = Math.floor(Math.random() * 5) + 1;
+    setTimeout(() => {
+      resolve(`Promise ${i + 1} resolved after ${randomTime} seconds.`);
+    }, randomTime * 1000);
+  }));
 }
 
-function promise2(){
-	const random = Math.floor(Math.random() *  5) + 1;
-	return new Promise((resolve,reject)=>{
-		setTimeout(()=>{
-		resolve("Promise 2")
-	    },random)
-	})
-}
+Promise.any(promises)
+  .then((result) => {
+    const outputDiv = document.getElementById('output');
+    outputDiv.textContent = result;
 
-function promise3(){
-	const random = Math.floor(Math.random() *  5) + 1;
-	return new Promise((resolve,reject)=>{
-		setTimeout(()=>{
-		resolve("Promise 3")
-	    },random)
-	})
-}
-
-function promise4(){
-	const random = Math.floor(Math.random() *  5) + 1;
-	return new Promise((resolve,reject)=>{
-		setTimeout(()=>{
-		resolve("Promise 4")
-	    },random)
-	})
-}
-
-function promise5(){
-	const random = Math.floor(Math.random() *  5) + 1;
-	return new Promise((resolve,reject)=>{
-		setTimeout(()=>{
-		resolve("Promise 5")
-	    },random)
-	})
-}
+    // Check the result of the resolved promise
+    const promiseIndex = promises.findIndex((promise) => promise[Symbol.for('PromiseState')] === 'fulfilled');
+    console.log(`Promise ${promiseIndex + 1} was the first to resolve with the result: ${result}`);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
 
 // Do not change the code above this
 // add your promises to the array `promises`
